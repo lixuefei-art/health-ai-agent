bad96918ce5375a8ffbb5b798e51b599d4fc10e7
lxf <1347996482@qq.com>
2026-03-30 09:58:07 +0800

    初始化后端项目

---
+++ src/main/java/com/lxf/healthaiagent/HealthAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lxf.healthaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxf/healthaiagent/HealthAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxf.healthaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
